5948e9793283ac5774fd13b83532407a095b4435
hippyclipper <cameron_moon321@hotmail.com>
2022-03-27 14:42:17 -0700

    working spin

--- src/cirlces.py
+++ src/cirlces.py
@@ -17,10 +17,11 @@ clock = pygame.time.Clock()
 
 class Circle:
     
-    def __init__(self,x,y,r):
+    def __init__(self,x,y,r, rads):
         self.x = x
         self.y = y
         self.r = r
+        self.rads = rads
         
     def draw(self):
         pygame.draw.circle(screen, BLUE, (self.x, self.y), self.r)
@@ -29,7 +30,8 @@ class Circles:
     
     def __init__(self):
         self.circles = []
-        self.numCircles = 3
+        self.numCircles = 10
+        self.bigRad = 100
         r = 20
         
         for i in range(self.numCircles):
@@ -37,9 +39,15 @@ class Circles:
             radians = math.radians(degree)
             centerX = width//2
             centerY = height//2
-            x = centerX + (math.cos(radians) * 100)          
-            y = centerY + (math.sin(radians) * 100)
-            self.circles.append(Circle(x,y,r))
+            x = centerX + (math.cos(radians) * self.bigRad)          
+            y = centerY + (math.sin(radians) * self.bigRad)
+            self.circles.append(Circle(x,y,r, radians))
+            
+    def update(self):
+        for x in self.circles:
+            x.rads += .01
+            x.x = width//2 + math.cos(x.rads)*self.bigRad
+            x.y = height//2 + math.sin(x.rads)*self.bigRad
 
     def draw(self):
         for x in self.circles:
@@ -53,6 +61,7 @@ while not done:
             done = True
             
     circles.draw()
+    circles.update()
     pygame.display.flip()
     clock.tick(60)
     screen.fill(BACKGROUND)
