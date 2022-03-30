6bca9b33fcc4bde577723aeec2aada38942ed973
hippyclipper <cameron_moon321@hotmail.com>
2022-03-29 22:50:17 -0700

    working explosion particles

--- src/fireworks.py
+++ src/fireworks.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 screenScale = 8
 width = int(100 * screenScale)
@@ -15,6 +16,37 @@ GREEN = (0,255,0)
 BACKGROUND = (5, 5, 5)
 done = False
 
+class Spark:
+    
+    def __init__(self):
+        v = random.randint(1,10)
+        self.xv = random.uniform(-v,v)
+        self.yv = math.sqrt(v**2 - self.xv**2) * [1,-1][random.randint(0,1)]
+        self.y = 0
+        self.x = 0
+        self.g = .1
+        self.decell = random.uniform(.90,.95)
+        self.r = 10 * random.uniform(.5,.1)
+        self.color = RED
+        self.timer = self.r
+        self.timerDelta = .1
+        self.isDone = False
+        
+    def update(self):
+        
+        self.x += self.xv
+        self.y += self.yv
+        self.yv += self.g
+        self.xv *= self.decell
+        self.yv *= self.decell
+        self.timer -= self.timerDelta
+        if self.timer < 0:
+            self.isDone = True
+        
+    def draw(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+        
+
 class Firework:
     
     def __init__(self):
@@ -26,26 +58,41 @@ class Firework:
         self.color = BLUE
         self.exploded = False
         self.explodeV = 5
+        self.sparks = [Spark() for x in range(100)]
+        
         
     def update(self):
 
-        if self.v > self.explodeV:
+        if self.v > self.explodeV and not self.exploded:
             self.exploded = True
             self.color = RED
-            
-        self.v += self.g
-        self.y += self.v
+            for spark in self.sparks:
+                spark.x = self.x
+                spark.y = self.y
+        if not self.exploded:  
+            self.v += self.g
+            self.y += self.v
+        else:
+            for spark in self.sparks:
+                spark.update()
+            for spark in self.sparks:
+                if spark.isDone:
+                    self.sparks.remove(spark)
         
     def isDone(self):
         
         if self.y < 0-self.r or (self.v > 0 and self.y > height):
             return True
+        return len(self.sparks) == 0
         
     def draw(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
-        
+        if not self.exploded:
+            pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+        else:
+            for spark in self.sparks:
+                spark.draw()
 
-fireworks = [Firework() for x in range(3)]
+fireworks = [Firework() for x in range(6)]
 
 while not done:
     
