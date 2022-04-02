cbaba06c59b94b44f4727f5c89baef238661e299
hippyclipper <cameron_moon321@hotmail.com>
2022-04-01 19:21:15 -0700

    started gravity + template

---
+++ src/gravity.py
@@ -0,0 +1,34 @@
+import pygame
+import random
+import math
+
+screenScale = 8
+width = int(100 * screenScale)
+height = width
+pygame.init()
+pygame.mixer.init()
+pygame.font.init()
+screen = pygame.display.set_mode((width,height))
+clock = pygame.time.Clock()
+RED = (255,0,0)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
+BACKGROUND = (5, 5, 5)
+COLORLIST = [RED, GREEN, BLUE]
+done = False
+
+while not done:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            
+    pygame.draw.circle(screen, RED, (width//2, height//2), 100)      
+            
+    pygame.display.flip()
+    clock.tick(60)
+    screen.fill(BACKGROUND)
+    
+#=============================================================
+pygame.display.quit()
+pygame.quit()         
